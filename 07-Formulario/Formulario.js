c2c039217cfcf42e79e67a86e42806c7e263b3b1
@if.dev_ <iara.freitas.job@gmail.com>
2024-06-26 10:37:27 -0300

    update

---
+++ 07-Formulario/Formulario.js
@@ -0,0 +1,23 @@
+//neste bloco estamos recuperando os botões
+const hamburgerButton = document.querySelector('#hamburguerButton');
+
+const closeButton = document.querySelector('#closeButton');
+
+const mobileMenu = document.querySelector('#mobileMenu');
+
+//....
+hamburgerButton.addEventListener('click', function(){
+    mobileMenu.classList.add('flex');
+});
+
+closeButton.addEventListener('click', function(){
+    mobileMenu.classList.remove('flex');
+});
+
+
+window.revelar = ScrollReveal({reset:true});
+
+revelar.reveal('.efeito-img-topo', {
+  duration: 2000,
+  distance: '90px'
+});
