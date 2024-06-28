b9a15ee37322be01452f658aa37b70b495d84c50
@if.dev_ <iara.freitas.job@gmail.com>
2024-06-28 13:37:24 -0300

    up

--- 02-PlanoDeNegocios/PlanoDeNegocios.js
+++ 02-PlanoDeNegocios/PlanoDeNegocios.js
@@ -13,3 +13,21 @@ hamburgerButton.addEventListener('click', function(){
 closeButton.addEventListener('click', function(){
     mobileMenu.classList.remove('flex');
 });
+
+
+//configuração do PAUSA/PLAY do vídeo no Bloco-01
+const iframe = document.getElementById("myVideo");  //Estamos fazendo uma busca dentro do HTML para salvar o ID
+const button = document.getElementById("myBtn");
+
+function myFunction() { //Aqui criamos uma Função 
+  
+if (iframe.paused) { //se o vídeo estiver pausado execute: Play.
+    iframe.play();
+    button.innerHTML = "Pause"; // O innerHTML funciona como uma alteração textual dentro do HTML, usamos ele no Javascript para executar essa chamada.
+  } else { //caso contrário, se o 
+    iframe.pause();
+    button.innerHTML = "Play";
+  }
+}
+
+
